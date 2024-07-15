938767fbe43b095c26a8c53613b13b0b64560364
Sairam1924 <145088258+Sairam1924@users.noreply.github.com>
2024-07-15 16:38:00 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
+import sklearn
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Load the pre-trained model
+model = pickle.load(open(r"C:\Users\padru\nb.pkl",'rb'))
+
+# Load the CountVectorizer used for training
+##with  as f:
+bow = pickle.load(open(r"C:\Users\padru\bow.pkl",'rb'))
+
+st.title("Email Spam/Ham Classifier")
+
+# Input email text
+Email = st.text_input("Paste the email here:")
+
+# Check if the email input is not empty
+if Email:
+
+    data = bow.transform([Email]).toarray()
+
+
+    spam_ham = model.predict(data)[0]
+
+
+    if st.button('Submit'):
+        st.write("The email is:",spam_ham)
